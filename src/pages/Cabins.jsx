17b6b5480d27092a539469211d810d6f4106be3d
HassaaNaeem <hassaan282006@gmail.com>
2026-04-09 20:23:16 +0500

    Handled create cabin form using react-hook-form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import Button from "../ui/Button";
 
 function Cabins() {
+  const [showForm, setShowFrom] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -11,6 +15,11 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+
+        <Button onClick={() => setShowFrom((show) => !show)}>
+          {!showForm ? "Add new cabin" : "Close Form"}
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
