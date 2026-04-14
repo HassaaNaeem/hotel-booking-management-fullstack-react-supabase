b86518e28e68254a05b5186109264d061b1ea538
HassaaNaeem <hassaan282006@gmail.com>
2026-04-14 23:47:45 +0500

    Added selection sorting options

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TableOperations from "../../ui/TableOperations";
 import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
 
 function CabinTableOperations() {
   return (
@@ -11,6 +12,19 @@ function CabinTableOperations() {
           { value: "all", label: "All" },
           { value: "no-discount", label: "No Discount" },
           { value: "with-discount", label: "With Discount" },
+        ]}
+      />
+      <SortBy
+        options={[
+          { value: "name-asc", label: "Sort by name (A-Z)" },
+          { value: "name-desc", label: "Sort by name (Z-A)" },
+          { value: "regularPrice-asc", label: "Sort by price (Low First)" },
+          {
+            value: "nameregularPrice-desc",
+            label: "Sort by price (High First)",
+          },
+          { value: "maxCapacity-asc", label: "Sort by capacity (Low First)" },
+          { value: "maxCapacity-desc", label: "Sort by name (High First)" },
         ]}
       />
     </TableOperations>
