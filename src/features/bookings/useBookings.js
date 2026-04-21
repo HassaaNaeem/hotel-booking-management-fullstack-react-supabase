248b5294b60918a7d53861e551820409aeb36392
HassaaNaeem <hassaan282006@gmail.com>
2026-04-21 23:21:36 +0500

    API side filtering bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAllBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue == "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // : { field: "status", value: filterValue, method: 'gte' };
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getAllBookings,
+    queryKey: ["bookings", filter], // whenever filter changes, react query will refetch the data
+    queryFn: () => getAllBookings({ filter }),
   });
   console.log(bookings);
   return { isLoading, error, bookings };
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,19 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getAllBookings() {
-  const { data, error } = await supabase
+export async function getAllBookings({ filter, sortyBy }) {
+  let query = supabase
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)",
     ); // getting only selected fields, and name from cabins table and fullName, email from guests table
+
+  // Filter
+
+  if (filter !== null)
+    query = query[filter.method || "eq"](filter.field, filter.value);
+
+  const { data, error } = await query;
 
   if (error) {
     console.log(error);
