c2de612a04f9a90f05c484f895df6a46ee10fcb1
HassaaNaeem <hassaan282006@gmail.com>
2026-05-04 16:23:58 +0500

    Prefetching with react query

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getAllBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   // FILTER
@@ -27,6 +29,19 @@ export function useBookings() {
   });
   const bookings = data?.data;
   const count = data?.count;
+
+  // PRE FETCHING
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1], // whenever filter changes, react query will refetch the data
+      queryFn: () => getAllBookings({ filter, sortBy, page: page + 1 }),
+    });
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1], // whenever filter changes, react query will refetch the data
+      queryFn: () => getAllBookings({ filter, sortBy, page: page - 1 }),
+    });
 
   return { isLoading, error, bookings, count };
 }
